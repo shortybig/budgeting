9426f8aeab331e9727b76c7221fea0b2318cd27e
shortybig <jbrannock2018@gmail.com>
2024-04-27 23:40:30 -0400

    transactions retrieved with Tiller, recategorized lunchdrop as a first start, loaded dataframe into Google sheets template provided by Tiller

--- budgeting.R
+++ budgeting.R
@@ -3,7 +3,7 @@ loadNamespace("tidyverse")
 
 
 # extract -----------------------------------------------------------------
-
+# currently using Tiller, but I'd like to eventually switch to using Plaid APIs
 googlesheets4::gs4_auth(email = "jbrannock2018@gmail.com")
 
 transactions <- 
@@ -16,11 +16,26 @@ transactions <-
 
 recategorizing <- 
   transactions |> 
+  dplyr::rename(
+    "subcategory" = "Category"
+  ) |> 
   dplyr::mutate(
-    Category = dplyr::case_when(
-      grepl("lunchdrop", Description, ignore.case = TRUE) ~ "lunch"
+    subcategory = dplyr::case_when(
+      grepl("lunchdrop", Description, ignore.case = TRUE) ~ "lunchdrop",
+      TRUE ~ "unknown"
     )
-  )
+  ) |> 
+  dplyr::mutate(
+    category = dplyr::case_when(
+      subcategory %in% c("lunchdrop") ~ "food",
+      TRUE ~ "other"
+    )
+  ) |> 
+  dplyr::select(-c(Account, `Account #`, `Transaction ID`, `Account ID`, `Check Number`, `Date Added`, Month, Week)) |> 
+  dplyr::mutate(
+    Date = format(Date, format = "%Y-%m-%d")
+  ) |>
+  dplyr::arrange(desc(Date))
 
 # load --------------------------------------------------------------------
 
